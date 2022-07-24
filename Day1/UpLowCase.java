6012214a034aca9218c67fc6c58278e37faac599
Jayant-ukey <66780779+Jayant-ukey@users.noreply.github.com>
2022-07-24 19:35:02 +0530

    Update UpLowCase.java
    First day 6th program

--- Day1/UpLowCase.java
+++ Day1/UpLowCase.java
@@ -1,3 +1,16 @@
+/*
+	Initialize a character variable with an alphabhet in a program.
+	If the character value is in lowercase, the output should be displayed in uppercase in the following format.
+	Example1)
+	i/p:a
+	o/p:a->A
+
+	If the character value is in uppercase, the output should be displayed in lowercase in the following format.
+	Example2)
+	i/p:A
+	o/p:A->a
+*/
+
 import java.util.Scanner;
 
 public class UpLowCase {
