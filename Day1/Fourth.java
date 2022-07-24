75a08ed71f5703cc4d131a1bb7e1b827ff8a7ac3
Jayant-ukey <66780779+Jayant-ukey@users.noreply.github.com>
2022-07-24 19:32:19 +0530

    Update Fourth.java
    First day fourth program

--- Day1/Fourth.java
+++ Day1/Fourth.java
@@ -1,3 +1,10 @@
+/*
+	Initialize a character variable in a program and
+	print 'Alphabhet' if the initialized value is an alphabhet,
+	print 'Digit' if the initialized value is a number, and
+	print 'Special Character', if the initialized value is anything else.
+*/
+
 import java.util.Scanner;
 
 public class Fourth {
