46f83f4d15e96df7389758a64ae8f814b3666c8e
Jayant-ukey <66780779+Jayant-ukey@users.noreply.github.com>
2022-07-27 13:44:24 +0530

    Create Demo1.java
    Junit 1st hands-on

---
+++ Day8/H1/Demo1.java
@@ -0,0 +1,16 @@
+/*
+ 	Class Name: Demo1
+	Method: String stringConcat(String,String)
+	[ returns concatenation of the 2 Strings received ]
+	Create the above class and method and test it using JUnit.
+*/
+
+
+public class Demo1 {
+
+	String stringConcat(String s1,String s2) {
+		
+		return s1+s2;
+	}
+	
+}
